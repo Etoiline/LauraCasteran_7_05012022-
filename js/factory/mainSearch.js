aa84b598f94ebb3ef1957e73f5d062381e48ef99
Laura Casteran <laurac644@gmail.com>
2022-02-09 13:36:33 +0100

    Correction bug

--- js/factory/mainSearch.js
+++ js/factory/mainSearch.js
@@ -44,7 +44,7 @@ export default class MainSearch {
       } else {
         this.filteredRecipes = recipesArray.slice()
       }
-      displayFilteredRecipes(this.filteredRecipes)
+      displayFilteredRecipes()
     })
   }
 }
